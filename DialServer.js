16b793e14792cf84c19b1f0a2dc1f9bd7f1ada4c
kevinatown <kevinatown@gmail.com>
2017-11-21 09:31:36 -0500

    updated changes to dial

--- DialServer.js
+++ DialServer.js
@@ -23,7 +23,7 @@ var apps = {
 	        var url = "http://www.youtube.com/tv?"+launchData;
 	        child = spawn('npm', ['start', url, config.position, config.width, config.height], {
   				cwd: 'modules/MMM-Screencast'
-			})
+			});
 			child.stdout.on('data', function(data) {
 			    console.log('screencast stdout: ' + data);
 			});
@@ -35,28 +35,6 @@ var apps = {
 			});
     	}
 	},
-	"Hulu": {
-		name: "Hulu",
-		state: "stopped",
-		allowStop: true,
-		pid: null,
-	    launch: function (launchData, config) {
-	    	console.log(launchData);
-	  //       var url = "http://www.youtube.com/tv?"+launchData;
-	  //       child = spawn('npm', ['start', url, config.position, config.width, config.height], {
-  	// 			cwd: 'modules/MMM-Screencast'
-			// })
-			// child.stdout.on('data', function(data) {
-			//     console.log('screencast stdout: ' + data);
-			// });
-			// child.stderr.on('data', function(data) {
-			//     console.log('screencast stderr: ' + data);
-			// });
-			// child.on('close', function(code) {
-			//     console.log('closing code: ' + code);
-			// });
-   //  	}
-	}
 };
 
 var dialServer = new dial.Server({
